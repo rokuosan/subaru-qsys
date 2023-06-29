4ee74691c74ab1169621bbc66918b233f940c68c
rokuosan <85651386+rokuosan@users.noreply.github.com>
2023-06-29 15:18:45 +0000

    Add some views

---
+++ qsys/app/urls.py
@@ -0,0 +1,16 @@
+from django.contrib.auth.views import LoginView, LogoutView
+from django.urls import path
+
+from .views.index import index
+
+
+urlpatterns = [
+    path('', index, name='index'),
+
+    # Login/Logout
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('login/', LoginView.as_view(
+        redirect_authenticated_user=True,
+        template_name='app/login.html'
+    ), name='login'),
+]
