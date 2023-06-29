216b55508697b2e2813ca48f2996c72472293be4
rokuosan <85651386+rokuosan@users.noreply.github.com>
2023-06-29 11:08:36 +0000

    Configure settings

--- qsys/qsys/settings.py
+++ qsys/qsys/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-gyci5g38kv6)#81pm-fqt5jxja1pkkytc(&ko2elr9&37q83%0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -103,9 +103,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = "ja-jp"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "Asia/Tokyo"
 
 USE_I18N = True
 
@@ -116,6 +116,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
