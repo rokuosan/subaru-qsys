28a65729d7f5bb1909b91882d2306ef845365a04
rokuosan <85651386+rokuosan@users.noreply.github.com>
2023-06-29 11:14:48 +0000

    Support django prometheus

--- qsys/qsys/settings.py
+++ qsys/qsys/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = "qsys.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
+        "ENGINE": "django_prometheus.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
