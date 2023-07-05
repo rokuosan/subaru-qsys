a1659043e6a3194eac5c7c90c9e9fb6183a1f22d
rokuosan <85651386+rokuosan@users.noreply.github.com>
2023-07-05 08:24:18 +0000

    Update account.py

--- qsys/app/views/account.py
+++ qsys/app/views/account.py
@@ -10,15 +10,28 @@ def account(request: HttpRequest):
     ctx = {}
 
     # Get Answers
-    answers = CtfAnswerHistory.objects.filter(user=request.user)
+    answers_original = CtfAnswerHistory.objects.filter(user=request.user)
+    answers_original = answers_original.order_by("-answered_at")
     # Last newest 10 answers
-    answers = answers.order_by("-answered_at")[:10]
+    answers = answers_original[:10]
 
-    print(answers)
+    # Your point
+    point = 12578232
+    # point = sum([answer.question.point for answer in answers_original])
+
+    # Answer ratio
+    length = len(answers_original)
+    if length == 0:
+        ratio = 0
+    else:
+        # ratio = point / length
+        ratio = point / 125782320
 
     ctx['display'] = {
         'username': request.user.username,
         'answers': answers,
+        'point': point,
+        'ratio': ratio
     }
 
     return render(request, 'app/account.html', ctx)
