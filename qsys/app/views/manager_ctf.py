791a04cc7cb48018c3a24f884a0cdeb063cc8c76
rokuosan <85651386+rokuosan@users.noreply.github.com>
2023-07-07 12:14:01 +0000

    WIP | manager_ctf.py

--- qsys/app/views/manager_ctf.py
+++ qsys/app/views/manager_ctf.py
@@ -1,8 +1,11 @@
+from datetime import datetime
+from django.utils import timezone
 from django.http import HttpRequest
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import redirect
+from django.shortcuts import redirect, render
 
 from app.views.index import index
+from app.models.ctf_information import CtfInformation
 
 
 @login_required
@@ -11,4 +14,22 @@ def manager_ctf(request: HttpRequest):
     if not user.is_admin:
         return redirect(index)
 
-    return redirect(index)
+    ctx = {}
+
+    ctfs = CtfInformation.objects.all()
+
+    now = datetime.now(timezone.utc)
+
+    upcoming_ctf = ctfs.filter(start_at__gte=now)
+    if upcoming_ctf.exists():
+        ctx['upcoming'] = upcoming_ctf
+
+    past_ctf = ctfs.filter(end_at__lte=now)
+    if past_ctf.exists():
+        ctx['past'] = past_ctf
+
+    current_ctf = ctfs.filter(start_at__lte=now, end_at__gte=now)
+    if current_ctf.exists():
+        ctx['current'] = current_ctf
+
+    return render(request, "app/manager_ctf.html", ctx)
