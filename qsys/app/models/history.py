1f2019e01aa02abeebfeb96f5a76185749fb70e6
rokuosan <85651386+rokuosan@users.noreply.github.com>
2023-07-07 09:49:12 +0000

    Update history model

--- qsys/app/models/history.py
+++ qsys/app/models/history.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django_prometheus.models import ExportModelOperationsMixin
 
+from .team import CtfTeam
 from .app_user import AppUser
 from .question import CtfQuestion
 
@@ -13,6 +14,7 @@ class CtfAnswerHistory(
     history_id = models.BigAutoField(primary_key=True)
     question = models.ForeignKey(CtfQuestion, models.CASCADE)
     user = models.ForeignKey(AppUser, models.CASCADE)
+    team = models.ForeignKey(CtfTeam, models.CASCADE, null=True, blank=True)
 
     content = models.CharField(max_length=1023, help_text="回答内容")
     is_correct = models.BooleanField(default=False, help_text="正解フラグ")
