2d585c91b1b3505d5f3a8d4bda45f3d3feb05aea
rokuosan <85651386+rokuosan@users.noreply.github.com>
2023-07-08 05:48:17 +0000

    WIP | ctf manager

--- qsys/app/views/manager_ctf.py
+++ qsys/app/views/manager_ctf.py
@@ -14,22 +14,34 @@ def manager_ctf(request: HttpRequest):
     if not user.is_admin:
         return redirect(index)
 
-    ctx = {}
+    if request.method == "GET":
+        ctx = {}
 
-    ctfs = CtfInformation.objects.all()
+        ctfs = CtfInformation.objects.all()
 
-    now = datetime.now(timezone.utc)
+        now = datetime.now(timezone.utc)
 
-    upcoming_ctf = ctfs.filter(start_at__gte=now)
-    if upcoming_ctf.exists():
-        ctx["upcoming"] = upcoming_ctf
+        upcoming_ctf = ctfs.filter(start_at__gte=now)
+        if upcoming_ctf.exists():
+            ctx["upcoming"] = upcoming_ctf
 
-    past_ctf = ctfs.filter(end_at__lte=now)
-    if past_ctf.exists():
-        ctx["past"] = past_ctf
+        past_ctf = ctfs.filter(end_at__lte=now)
+        if past_ctf.exists():
+            ctx["past"] = past_ctf
 
-    current_ctf = ctfs.filter(start_at__lte=now, end_at__gte=now)
-    if current_ctf.exists():
-        ctx["current"] = current_ctf
+        current_ctf = ctfs.filter(start_at__lte=now, end_at__gte=now)
+        if current_ctf.exists():
+            ctx["current"] = current_ctf
 
-    return render(request, "app/manager_ctf.html", ctx)
+        return render(request, "app/manager_ctf.html", ctx)
+
+    elif request.method == "POST":
+        if "start" in request.POST:
+            ctf_id = request.POST.get("id")
+            ctf = CtfInformation.objects.get(pk=ctf_id)
+            ctf.is_active = True
+            ctf.save()
+
+            return redirect(manager_ctf)
+
+    return redirect(index)
