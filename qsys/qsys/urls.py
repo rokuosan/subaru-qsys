049d51be83638b190bb6d546f456b5b564e38645
rokuosan <85651386+rokuosan@users.noreply.github.com>
2023-07-09 14:00:07 +0000

    Update urls.py to support django_prometheus

--- qsys/qsys/urls.py
+++ qsys/qsys/urls.py
@@ -20,4 +20,5 @@ from django.urls import include, path
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("app.urls")),
+    path("", include("django_prometheus.urls")),
 ]
