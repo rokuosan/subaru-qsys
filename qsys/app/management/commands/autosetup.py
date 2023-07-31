4b49343b39bb95cdf70ba5233d4505e5b6ff6f5e
rokuosan <85651386+rokuosan@users.noreply.github.com>
2023-07-31 10:13:03 +0000

    Issue #4 | Support User add

--- qsys/app/management/commands/autosetup.py
+++ qsys/app/management/commands/autosetup.py
@@ -1,3 +1,4 @@
+import random
 from django.core.management.base import BaseCommand
 import yaml
 
@@ -105,15 +106,103 @@ class Command(BaseCommand):
         print("Questions Done.")
         print()
 
+    def setup_users(self, yml: dict):
+        data = yml["user"]
+        pw_list = []
+
+        roughed = data.get("roughed", None)
+        if roughed is not None and type(roughed) is list:
+            print("Setting up Roughed Users...")
+            for username in roughed:
+                print(f"- {username}", end=" ")
+                try:
+                    u = AppUser.objects.filter(username=username)
+                    if u.exists():
+                        print("Already exists")
+                        continue
+                    pw = AppUser.objects.make_random_password()
+                    pw_list.append((username, pw))
+                    u = AppUser(username=username)
+                    u.set_password(pw)
+                    u.save()
+                    print("OK")
+                except Exception as e:
+                    print("Failed")
+                    print(e)
+                    return
+
+        detailed = data.get("detailed", None)
+        if detailed is not None and type(detailed) is list:
+            print("Setting up Detailed Users...")
+            for user in detailed:
+                print(f"- {user['username']}", end=" ")
+                try:
+                    # Check
+                    u = AppUser.objects.filter(username=user["username"])
+                    if u.exists():
+                        print("Already exists")
+                        continue
+
+                    # Password
+                    pw = user.get("password", None)
+
+                    # Team
+                    team = user.get("team", None)
+                    if team is not None:
+                        try:
+                            team = CtfTeam.objects.get(name=team)
+                        except CtfTeam.DoesNotExist:
+                            print("Team not found")
+                            continue
+
+                    # Flags
+                    is_admin = user.get("is_admin", False)
+                    is_staff = user.get("is_staff", False)
+                    is_superuser = user.get("is_superuser", False)
+                    is_active = user.get("is_active", True)
+
+                    # Create
+                    u = AppUser(
+                        username=user["username"],
+                        team=team,
+                        is_admin=is_admin,
+                        is_staff=is_staff,
+                        is_superuser=is_superuser,
+                        is_active=is_active,
+                    )
+
+                    if pw is not None:
+                        u.set_password(pw)
+                    else:
+                        pw = AppUser.objects.make_random_password()
+                        pw_list.append((user["username"], pw))
+                        u.set_password(pw)
+                    u.save()
+                    print("OK")
+                except Exception as e:
+                    print("Failed")
+                    print(e)
+                    return
+
+        # Print password list as csv
+        csv = "\n".join([f"{u}, {p}" for u, p in pw_list])
+        rand = random.randrange(1000, 10000)
+        with open(f"passwords_{rand}.csv", "w") as f:
+            f.write(csv)
+
+        print("Users Done.")
+        print()
+
     def handle(self, *args, **options):
         data = self.read_yaml()
         if data is None:
             return
         qsys = data["qsys"]
 
-        self.simple_setup(qsys, "difficulty", CtfQuestionDifficulty)
-        self.simple_setup(qsys, "category", CtfQuestionCategory)
-        self.simple_setup(qsys, "team", CtfTeam)
-        self.setup_questions(qsys)
+        # self.simple_setup(qsys, "difficulty", CtfQuestionDifficulty)
+        # self.simple_setup(qsys, "category", CtfQuestionCategory)
+        # self.simple_setup(qsys, "team", CtfTeam)
+        # self.setup_questions(qsys)
+        self.setup_users(qsys)
 
         print("All Setup Completed.")
