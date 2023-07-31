926cf072debc44181128073ef553537549bf1d39
rokuosan <85651386+rokuosan@users.noreply.github.com>
2023-07-31 13:15:42 +0000

    Bug fix

--- qsys/app/views/ranking.py
+++ qsys/app/views/ranking.py
@@ -3,7 +3,6 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import redirect, render
 from app.models.ctf_information import CtfInformation
-from app.models.score import CtfScore
 from app.models.history import CtfAnswerHistory as history
 from app.forms.score_setting import ScoreSettingForm
 
@@ -14,7 +13,6 @@ def ranking(request: HttpRequest):
 
     ctx = {}
     ctfs = CtfInformation.objects.filter(is_active=True)
-    scores = CtfScore.objects.all()
 
     # CTFを取得
     if ctfs.count() == 0:
@@ -46,55 +44,44 @@ def ranking(request: HttpRequest):
         for t in teams
     ]
 
+    # スコアで降順にソート
+    team_scores = sorted(team_scores, key=lambda x: (-x["score"], x["name"]))
+
     # チームのスコアをもとにランキングを算出し、同値の場合は次の順位を飛ばす
-    rank = 1
+    rank = 0
     prev_score = -1
     for i, score in enumerate(team_scores):
         if score["score"] != prev_score:
-            rank = i + 1
+            rank += 1
         score["rank"] = rank
         prev_score = score["score"]
 
-    # チームごとのスコアを降順にソート
-    team_score_set = sorted(
-        team_scores, key=lambda x: (x["score"], x["name"]), reverse=True
-    )
-
     # ユーザーごとのスコアを算出
-    player_scores = []
-    for user in users:
-        user_score = 0
-        for score in scores:
-            if score.user == user:
-                user_score += score.point
-        player_scores.append(user_score)
     player_scores = [
         {"name": u.username, "score": history.get_user_point(u, ctf)}
         for u in users
     ]
-
+    # ソート
+    player_scores = sorted(
+        player_scores, key=lambda x: (-x["score"], x["name"])
+    )
     # ランキング処理
-    rank = 1
+    rank = 0
     prev_score = -1
     for i, score in enumerate(player_scores):
         if score["score"] != prev_score:
-            rank = i + 1
+            rank += 1
         score["rank"] = rank
         prev_score = score["score"]
-
-    # ユーザーごとのスコアを降順にソート
-    player_score_set = sorted(
-        player_scores, key=lambda x: (x["score"], x["name"]), reverse=True
-    )
 
     # ランキングを表示
     user = request.user
     if ctf.show_team_ranking or user.is_admin:
-        ctx["team_score_rank"] = team_score_set
+        ctx["team_score_rank"] = team_scores
         if not ctf.show_team_ranking:
             messages.info(request, "チームランキングは現在非公開です。管理者のみ表示されます。")
     if ctf.show_player_ranking or user.is_admin:
-        ctx["player_score_rank"] = player_score_set
+        ctx["player_score_rank"] = player_scores
         if not ctf.show_player_ranking:
             messages.info(request, "プレイヤーランキングは現在非公開です。管理者のみ表示されます。")
 
