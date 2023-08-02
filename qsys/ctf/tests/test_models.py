f8cf62f114196a036a6ff2fdb672788ebc55f021
rokuosan <85651386+rokuosan@users.noreply.github.com>
2023-08-02 01:30:35 +0000

    write test

---
+++ qsys/ctf/tests/test_models.py
@@ -0,0 +1,20 @@
+from django.utils import timezone
+from django.test import TestCase
+
+from ctf.models.ctf import CTF
+
+
+class CtfModelTests(TestCase):
+    def test_start_at(self):
+        time = timezone.now()
+        ctf = CTF.objects.create(
+            name="test",
+        )
+        self.assertLessEqual(time, ctf.start_at)
+
+    def test_end_at(self):
+        ctf = CTF.objects.create(
+            name="test",
+        )
+        print(ctf.start_at, ctf.end_at)
+        self.assertLessEqual(ctf.start_at, ctf.end_at)
