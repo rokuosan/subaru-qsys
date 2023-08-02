bc9badd41415d5e3de30e9fd1fa45b7fc1fe39e6
rokuosan <85651386+rokuosan@users.noreply.github.com>
2023-08-02 02:25:11 +0000

    register admin site

--- qsys/ctf/admin.py
+++ qsys/ctf/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
+from .models.ctf import CTF
+from .models.history import History
+from .models.player import Player
+from .models.question import Category, Difficulty, Question
+from .models.team import Team
+
 # Register your models here.
+admin.site.register(Player)
+admin.site.register(CTF)
+admin.site.register(History)
+admin.site.register(Question)
+admin.site.register(Category)
+admin.site.register(Difficulty)
+admin.site.register(Team)
