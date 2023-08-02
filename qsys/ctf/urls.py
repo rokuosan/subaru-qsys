e3a4ed05ec9688187c15bdd8260723e8c7a14c28
rokuosan <85651386+rokuosan@users.noreply.github.com>
2023-08-02 15:45:46 +0000

    add contest home

--- qsys/ctf/urls.py
+++ qsys/ctf/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from ctf.views.questions import questions_view
 from ctf.views.index import index_view
 from ctf.views.manager.contest import manager_contest_view
-from ctf.views.contest.index import contest_view
+from ctf.views.contest.home import contest_home_view
 
 
 app_name = "ctf"
@@ -14,7 +14,7 @@ urlpatterns = [
     path("", index_view, name="index"),
 
     # Contest
-    path("<str:contest_id>/", contest_view, name="contest"),
+    path("<str:contest_id>/", contest_home_view, name="home"),
     path("<str:contest_id>/questions/", questions_view, name="questions"),
 
     # Manager
---
+++ qsys/ctf/views/contest/home.py
@@ -0,0 +1,17 @@
+from django.http import HttpRequest
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import get_object_or_404, redirect, render
+from django.contrib import messages
+from ctf.models.contest import Contest
+
+
+@login_required
+def contest_home_view(request: HttpRequest, contest_id: str):
+    contest = get_object_or_404(Contest, id=contest_id)
+    ctx = {"contest": contest}
+    if not contest.is_open:
+        if not request.user.is_admin:
+            return redirect("ctf:index")
+        messages.info(request, "このコンテストは非公開です")
+
+    return render(request, "ctf/contest/home.html", ctx)
