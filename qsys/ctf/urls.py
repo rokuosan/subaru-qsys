2615c9c589903ac1a6c5193edac94d26a0d27f71
rokuosan <85651386+rokuosan@users.noreply.github.com>
2023-08-02 18:04:33 +0000

    add route

--- qsys/ctf/urls.py
+++ qsys/ctf/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from ctf.views.questions import questions_view
+from ctf.views.contest.questions import question_detail_view, questions_view
 from ctf.views.index import index_view
 from ctf.views.manager.contest import manager_contest_view
 from ctf.views.contest.home import contest_home_view
@@ -12,10 +12,14 @@ app_name = "ctf"
 urlpatterns = [
     # Index
     path("", index_view, name="index"),
-
     # Contest
     path("<str:contest_id>/", contest_home_view, name="home"),
     path("<str:contest_id>/questions/", questions_view, name="questions"),
+    path(
+        "<str:contest_id>/questions/<str:question_id>",
+        question_detail_view,
+        name="question_detail",
+    ),
 
     # Manager
     path("manager/contest/", manager_contest_view, name="manager_contest"),
