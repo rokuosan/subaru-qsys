b9bc789ebaa0c979cf7ec656403a8618435f26ac
rokuosan <85651386+rokuosan@users.noreply.github.com>
2023-08-02 01:00:16 +0000

    format

--- qsys/ctf/models/question.py
+++ qsys/ctf/models/question.py
@@ -6,20 +6,27 @@ from django_prometheus.models import ExportModelOperationsMixin
 
 class Question(models.Model, ExportModelOperationsMixin("question")):
     """CTF 問題"""
-    id = models.UUIDField(primary_key=True, help_text="問題ID", default=uuid.uuid4, editable=False)
+
+    id = models.UUIDField(
+        primary_key=True, help_text="問題ID", default=uuid.uuid4, editable=False
+    )
     title = models.CharField(max_length=255, help_text="問題タイトル")
     description = models.TextField(help_text="問題文", blank=True, default="")
     flag = models.CharField(max_length=255, help_text="フラグ")
     point = models.PositiveIntegerField(help_text="点数")
     category = models.ForeignKey(
-        "Category", on_delete=models.SET_DEFAULT, help_text="カテゴリ", default="Uncategorized"
+        "Category",
+        on_delete=models.SET_DEFAULT,
+        help_text="カテゴリ",
+        default="Uncategorized",
     )
     difficulty = models.ForeignKey(
-        "Difficulty", on_delete=models.SET_DEFAULT, help_text="難易度", default="Easy"
+        "Difficulty",
+        on_delete=models.SET_DEFAULT,
+        help_text="難易度",
+        default="Easy",
     )
-    is_open = models.BooleanField(
-        help_text="公開中かどうか", default=True
-    )
+    is_open = models.BooleanField(help_text="公開中かどうか", default=True)
     created_at = models.DateTimeField(auto_now_add=True, help_text="作成日時")
     updated_at = models.DateTimeField(auto_now=True, help_text="更新日時")
 
@@ -33,6 +40,7 @@ class Question(models.Model, ExportModelOperationsMixin("question")):
 
 class Category(models.Model, ExportModelOperationsMixin("category")):
     """CTF 問題カテゴリ"""
+
     name = models.CharField(max_length=255, help_text="カテゴリ名", unique=True)
 
     def __str__(self):
@@ -45,6 +53,7 @@ class Category(models.Model, ExportModelOperationsMixin("category")):
 
 class Difficulty(models.Model, ExportModelOperationsMixin("difficulty")):
     """CTF 問題難易度"""
+
     level = models.PositiveSmallIntegerField(help_text="難易度レベル", unique=True)
     name = models.CharField(max_length=255, help_text="難易度名", unique=True)
 
@@ -54,4 +63,3 @@ class Difficulty(models.Model, ExportModelOperationsMixin("difficulty")):
     class Meta:
         app_label = "ctf"
         verbose_name = verbose_name_plural = "問題難易度"
-
