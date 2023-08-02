1eb481ead1d6b3da865639cda1b5c56ada95c481
rokuosan <85651386+rokuosan@users.noreply.github.com>
2023-08-02 13:30:46 +0000

    update test

--- qsys/ctf/tests/test_models.py
+++ qsys/ctf/tests/test_models.py
@@ -1,3 +1,4 @@
+from django.urls import reverse
 from django.utils import timezone
 from django.test import TestCase
 
@@ -8,18 +9,50 @@ from ctf.models.team import Team
 
 
 class CtfModelTests(TestCase):
-    def test_start_at(self):
-        time = timezone.now()
-        ctf = Contest.objects.create(
-            id="test",
+    def setUp(self):
+        # Create User
+        self.user = AppUser.objects.create_user(
+            username="Test Tarou",
         )
-        self.assertLessEqual(time, ctf.start_at)
 
-    def test_end_at(self):
-        ctf = Contest.objects.create(
-            id="test",
+        # Create Player
+        self.player = Player.objects.create(
+            name="Test Tarou Player",
+            user=self.user,
         )
-        self.assertLessEqual(ctf.start_at, ctf.end_at)
+
+        # Create Team
+        self.team = Team.objects.create(
+            name="Test Team",
+        )
+        self.team.members.add(self.player)
+
+        # Create Contest
+        self.contest = Contest.objects.create(
+            id="test",
+            display_name="Test Contest",
+            description="test",
+            status=Contest.Status.RUNNING,
+            start_at=timezone.now(),
+            end_at=timezone.now() + timezone.timedelta(days=1),
+        )
+        self.contest.teams.add(self.team)
+
+    def test_player(self):
+        self.assertEqual(self.player.name, "Test Tarou Player")
+        self.assertEqual(self.player.user, self.user)
+
+    def test_team(self):
+        self.assertEqual(self.team.name, "Test Team")
+        self.assertEqual(self.team.members.first(), self.player)
+
+    def test_contest(self):
+        self.assertEqual(self.contest.id, "test")
+        self.assertEqual(self.contest.display_name, "Test Contest")
+        self.assertEqual(self.contest.description, "test")
+        self.assertEqual(self.contest.status, Contest.Status.RUNNING)
+        self.assertLessEqual(self.contest.start_at, self.contest.end_at)
+        self.assertEqual(self.contest.teams.first(), self.team)
 
     def test_reverse_reference(self):
         user = AppUser.objects.create(
@@ -29,9 +62,11 @@ class CtfModelTests(TestCase):
             name="test_player",
             user=user,
         )
-        user1 = AppUser.objects.create(
-            username="test_user1",
-        )
 
         self.assertEqual(player, user.player)
-        self.assertEqual(user1.player, None)
+
+    def test_manager_contest_view_redirect_check(self):
+        self.client.force_login(self.user)
+        response = self.client.get(reverse("ctf:manager_contest"))
+        print(response)
+        self.assertNotEqual(response.status_code, 200)
