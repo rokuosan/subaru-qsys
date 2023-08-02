f3ec7e1492a20856fde8151f0788f23f8b024dcd
rokuosan <85651386+rokuosan@users.noreply.github.com>
2023-08-02 14:06:05 +0000

    wip | parameter test

--- qsys/ctf/urls.py
+++ qsys/ctf/urls.py
@@ -2,12 +2,21 @@ from django.urls import path
 
 from ctf.views.questions import questions_view
 from ctf.views.index import index_view
+from ctf.views.manager.contest import manager_contest_view
+from ctf.views.contest.index import contest_view
+
+
+app_name = "ctf"
 
 
 urlpatterns = [
     # Index
     path("", index_view, name="index"),
 
-    # Questions
+    # Contest
+    path("<str:contest_id>/", contest_view, name="contest"),
     path("<str:contest_id>/questions/", questions_view, name="questions"),
+
+    # Manager
+    path("manager/contest/", manager_contest_view, name="manager_contest"),
 ]
