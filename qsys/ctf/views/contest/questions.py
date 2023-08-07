fd31dc9202385b9e20eca9bb95dcf9a6a6a28a95
rokuosan <85651386+rokuosan@users.noreply.github.com>
2023-08-07 16:33:07 +0000

    add question judgement

--- qsys/ctf/views/contest/questions.py
+++ qsys/ctf/views/contest/questions.py
@@ -1,3 +1,4 @@
+import re
 from django.http import HttpRequest
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404, redirect, render
@@ -82,6 +83,67 @@ def question_detail_view(
             return redirect("ctf:index")
 
     if request.method == "GET":
+        # セッションから結果を取得
+        ctx["result"] = request.session.get("result")
+        request.session["result"] = None
+
         return render(request, "ctf/contest/question_detail.html", ctx)
+
+    if request.method == "POST":
+        answer = request.POST.get("answer")
+        answer = re.sub(r"\s", "", answer)
+        if not answer or answer == "":
+            messages.warning(request, "回答が空です。判定を行いません。")
+            return redirect("ctf:question_detail", contest_id, question_id)
+
+        # 結果に表示する関数
+        def set_result(alert_type, message):
+            request.session["result"] = {
+                "alert_type": alert_type,
+                "message": message,
+            }
+
+        # 回答済み
+        if question.id in solved.values_list("question", flat=True):
+            set_result("warning", "既に回答済みです。")
+            return redirect("ctf:question_detail", contest_id, question_id)
+
+        team = contest.get_team_by_player(player)
+
+        if re.sub(r"\s", "", question.flag) == answer:
+            question_pts = 0
+            hist = History.objects.filter(
+                contest=contest, question=question, team=team, is_correct=True
+            )
+            if hist.count() == 0:
+                question_pts = question.point
+                set_result("success", "正解！")
+            else:
+                set_result("success", "正解！チームが解答済みのため点数は加算されません。")
+
+            # 正解履歴を保存
+            History.objects.create(
+                contest=contest,
+                team=team,
+                player=player,
+                question=question,
+                point=question_pts,
+                is_correct=True,
+            )
+
+            return redirect("ctf:question_detail", contest_id, question_id)
+        else:
+            # 正解履歴を保存
+            History.objects.create(
+                contest=contest,
+                team=team,
+                player=player,
+                question=question,
+                point=0,
+                is_correct=False,
+            )
+
+            set_result("warning", "不正解...")
+            return redirect("ctf:question_detail", contest_id, question_id)
 
     return render(request, "ctf/contest/questions.html", ctx)
