109c51238692d9a58a886c04098e5bf4e6fd5641
rokuosan <85651386+rokuosan@users.noreply.github.com>
2023-08-15 10:43:32 +0000

    add a config

--- qsys/qsys/settings.py
+++ qsys/qsys/settings.py
@@ -132,6 +132,7 @@ STATIC_URL = "static/"
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+MEDIA_ROOT = BASE_DIR / "media"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
