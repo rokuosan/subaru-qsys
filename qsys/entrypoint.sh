3e302561d85fd9d844495f5e75f42e4cd774fb7b
rokuosan <85651386+rokuosan@users.noreply.github.com>
2023-08-24 01:45:11 +0000

    Run autosetup on container startup

--- qsys/entrypoint.sh
+++ qsys/entrypoint.sh
@@ -6,5 +6,11 @@ python manage.py collectstatic --noinput
 # Apply database migrations
 python manage.py migrate
 
+# Auto setup
+python manage.py autosetup
+
+# Set write permission
+chmod 666 db.sqlite3
+
 # Start server
 uwsgi --ini uwsgi.ini --enable-threads
